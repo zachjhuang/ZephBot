6ac980f1bb7468ffa1ea723c77ee7dd2d6bee00f
Zach Huang <zhuo-nanhuang2024@u.northwestern.edu>
2024-10-11 16:52:05 -0400

    update template files

--- configs/roster_template.py
+++ configs/roster_template.py
@@ -1,122 +1,126 @@
-characters = [
+"""
+Currently supported classes for chaos dungeon are:
+    - 'aeromancer' (Drizzle)
+    - 'slayer' (Predator)
+    - 'artist' (Recurrence)
+    - 'deathblade' (Remaining Energy)
+    - 'arcana' (Emperor)
+    - 'scrapper' (Taijutsu)
+    - all other classes which do not rely on pressing specialty 
+      (Reflux 'sorceress', Perfect Suppression 'shadowhunter', Hunger 'reaper', etc.)
+
+If a class relies on pressing specialty and is not specifically listed above,
+clear speed may be less than average. Please submit an issue on GitHub to suggest 
+class implementations.
+
+'chaosItemLevel' must be a valid chaos dungeon item level
+'unas' is a string of up to three different task names,
+separated by spaces.
+Possible tasks include
+    - 'lopang' (Shushire, Arthetine, Vern, silver)
+    - 'mokomoko' (How to Succeed at Length, leapstones)
+    - 'bleakNightFog' (Bleak Night Fog, leapstones)
+    - 'hesteraGarden (Examine the Brilliant Spring, leapstones)
+    - 'sageTower' (Finding Valuable Resources, leapstones)
+    - 'southKurzan' (Preserve the ancient relic, leapstones)
+    - 'prehilia' (Befriend the Hiliaberry Thief!, shards)
+    - 'ghostStory (Birth of a Ghost Story, shards)
+    - 'writersLife' (Writer's Life: Fan Meeting, shards)
+
+MUST SET UP BIFROSTS ACCORDING TO EXAMPLE IN /unaexamples
+MEMO FOR BIFROST MUST BE THE EXACT STRING (CASE SENSITIVE)
+"""
+
+roster = [
     {
         "index": 0,
         "class": "souleater",
-        "ilvl-endless": 1630,
-        "ilvl-aor": 1630,
-        "unas": "leaps hesteraGarden sageTower southKurzan",
-        "guildDonation": True,
-        "guildSailingWeekly": 8,
-        "rapport": False,
+        "chaosItemLevel": None,
+        "unas": "prehilia writersLife ghostStory",
+        "guildDonation": True
     },
     {
         "index": 1,
         "class": "aeromancer",
-        "ilvl-endless": 1630,
-        "ilvl-aor": 1630,
-        "unas": "leaps hesteraGarden sageTower southKurzan",
-        "guildDonation": True,
-        "guildSailingWeekly": 8,
-        "rapport": False,
+        "chaosItemLevel": None,
+        "unas": None,
+        "guildDonation": True
     },
         {
         "index": 2,
         "class": "sorceress",
-        "ilvl-endless": 1610,
-        "ilvl-aor": 1610,
-        "unas": "leaps hesteraGarden sageTower southKurzan",
-        "guildDonation": True,
-        "guildSailingWeekly": 8,
-        "rapport": False,
+        "chaosItemLevel": None,
+        "unas": None,
+        "guildDonation": True
     },
     {
         "index": 3,
         "class": "gunslinger",
-        "ilvl-endless": 1600,
-        "ilvl-aor": 1600,
-        "unas": "leaps hesteraGarden sageTower southKurzan",
-        "guildDonation": True,
-        "guildSailingWeekly": 8,
-        "rapport": False,
+        "chaosItemLevel": 1610,
+        "unas": "hesteraGarden sageTower southKurzan",
+        "guildDonation": True
     },
     {
         "index": 4,
         "class": "glaivier",
-        "ilvl-endless": 1600,
-        "ilvl-aor": 1600,
-        "unas": "leaps hesteraGarden sageTower southKurzan",
-        "guildDonation": True,
-        "guildSailingWeekly": 8,
-        "rapport": False,
+        "chaosItemLevel": 1600,
+        "unas": "hesteraGarden sageTower southKurzan",
+        "guildDonation": True
     },
     {
         "index": 5,
         "class": "artist",
-        "ilvl-endless": 1600,
-        "ilvl-aor": 1600,
-        "unas": "leaps hesteraGarden sageTower southKurzan",
-        "guildDonation": True,
-        "guildSailingWeekly": 8,
-        "rapport": False,
+        "chaosItemLevel": 1600,
+        "unas": "hesteraGarden sageTower southKurzan",
+        "guildDonation": True
     },
     {
         "index": 6,
         "class": "deathblade",
-        "ilvl-endless": 1540,
-        "ilvl-aor": 1540,
+        "chaosItemLevel": 1560,
         "unas": "lopang",
-        "guildDonation": True,
-        "guildSailingWeekly": 2,
-        "rapport": False,
+        "guildDonation": True
     },
     {
         "index": 7,
         "class": "reaper",
-        "ilvl-endless": 1540,
-        "ilvl-aor": 1540,
+        "chaosItemLevel": 1540,
         "unas": "lopang",
-        "guildDonation": True,
-        "guildSailingWeekly": 2,
-        "rapport": False,
+        "guildDonation": True
     },
     {
         "index": 8,
         "class": "arcana",
-        "ilvl-endless": 1540,
-        "ilvl-aor": 1540,
+        "chaosItemLevel": 1540,
         "unas": "lopang",
-        "guildDonation": True,
-        "guildSailingWeekly": 2,
-        "rapport": False,
+        "guildDonation": True
     },
     {
         "index": 9,
         "class": "slayer",
-        "ilvl-endless": 1540,
-        "ilvl-aor": 1540,
+        "chaosItemLevel": 1540,
         "unas": "lopang",
-        "guildDonation": True,
-        "guildSailingWeekly": 2,
-        "rapport": False,
+        "guildDonation": True
     },
     {
         "index": 10,
         "class": "scrapper",
-        "ilvl-endless": 1540,
-        "ilvl-aor": 1540,
+        "chaosItemLevel": 1540,
         "unas": "lopang",
-        "guildDonation": True,
-        "guildSailingWeekly": 2,
-        "rapport": False,
+        "guildDonation": True
     },
     {
         "index": 11,
         "class": "shadowhunter",
-        "ilvl-endless": 1540,
-        "ilvl-aor": 1540,
+        "chaosItemLevel": 1540,
         "unas": "lopang",
-        "guildDonation": True,
-        "guildSailingWeekly": 2,
-        "rapport": False,
+        "guildDonation": True
+    },
+    {
+        "index": 12,
+        "class": "souleater",
+        "chaosItemLevel": 1540,
+        "unas": "bleakNightFog",
+        "guildDonation": True
     },
 ]
