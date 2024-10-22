521e5127b34ec3f7c33c5df8e845c872e9b729f6
Zach Huang <zhuo-nanhuang2024@u.northwestern.edu>
2024-10-22 03:36:25 -0400

    update constants, change pixel search to use distance instead of spiral search

--- modules/minimap.py
+++ modules/minimap.py
@@ -9,27 +9,9 @@ from modules.utilities import checkImageOnScreen, findImageCenter
 SCREEN_CENTER_X = 960
 SCREEN_CENTER_Y = 540
 
-MINIMAP_REGION = (1655, 170, 240, 200)
-MINIMAP_CENTER_X = 1772
+MINIMAP_REGION = (1653, 172, 240, 200)
+MINIMAP_CENTER_X = 1773
 MINIMAP_CENTER_Y = 272
-
-MOB_RGB_RANGE_GFN = lambda r, g, b: 180 < r < 215 and 17 < g < 35 and 17 < b < 55
-MOG_RGB_RANGE = lambda r, g, b: 180 < r < 215 and 17 < g < 35 and 17 < b < 55
-
-ELITE_RGB_RANGE_GFN = lambda r, g, b: 184 < r < 215 and 124 < g < 147 and 59 < b < 78
-ELITE_RGB_RANGE = lambda r, g, b: 189 < r < 215 and 124 < g < 150 and 29 < b < 70
-
-BUFF_RGB_RANGE_GFN = lambda r, g, b: 210 < r < 255 and 170 < g < 190 and 30 < b < 60
-BUFF_RGB_RANGE = lambda r, g, b: 189 < r < 215 and 124 < g < 150 and 29 < b < 70
-
-PORTAL_RGB_RANGE_GFN = lambda r, g, b: (
-    (75 < r < 105 and 140 < g < 170 and 240 < b < 256)
-    or (120 < r < 130 and 210 < g < 240 and 240 < b < 256)
-)
-PORTAL_RGB_RANGE = lambda r, g, b: (
-    (75 < r < 85 and 140 < g < 150 and 250 < b < 256)
-    or (120 < r < 130 and 210 < g < 220 and 250 < b < 256)
-)
 
 
 class Minimap:
@@ -41,30 +23,55 @@ class Minimap:
         self, name: str, inColorRange: Callable[[int, int, int], bool]
     ) -> bool:
         """
-        Check minimap for closest pixel that satisfies the range lambda given.
-        Return true if found and update minimap target coordinates.
-        Return false otherwise.
+        Check minimap for closest pixel that satisfies the color range lambda given.
+
+        Range lambda should take `r`, `g`, and `b` values and return True if each value is
+        within a certain threshold.
+
+        Updates `targets` attribute if found.
+
+        This function is intended for internal use only.
+
+        Returns:
+            `True` if found, `False` otherwise.
         """
         minimap = pyautogui.screenshot(region=MINIMAP_REGION)
         width, height = minimap.size
-        order = spiralSearch(
-            width, height, math.floor(width / 2), math.floor(height / 2)
-        )
-        for entry in order:
-            if entry[0] >= width or entry[1] >= height:
-                continue
-            r, g, b = minimap.getpixel((entry[0], entry[1]))
-            if inColorRange(r, g, b):
-                left, top, _w, _h = MINIMAP_REGION
-                self.targetX = left + entry[0] - MINIMAP_CENTER_X
-                self.targetY = top + entry[1] - MINIMAP_CENTER_Y
-                # print(f"{name} pixel at x:{self.targetX} y:{self.targetY}")
-                return True
-        self.targetX = 0
-        self.targetY = 0
-        return False
 
-    def getGameCoordsOfMinimapTarget(self) -> tuple[int, int, int]:
+        closest_x = 0
+        closest_y = 0
+        closest_dist = 200
+        for x in range(width):
+            for y in range(height):
+                r, g, b = minimap.getpixel((x, y))
+                if inColorRange(r, g, b):
+                    left, top, _w, _h = MINIMAP_REGION
+                    dx = left + x - MINIMAP_CENTER_X
+                    dy = top + y - MINIMAP_CENTER_Y
+                    if math.sqrt(dx**2 + dy**2) < closest_dist:
+                        closest_x = dx
+                        closest_y = dy
+                        closest_dist = math.sqrt(dx**2 + dy**2)
+        if closest_dist < 200:
+            print(f"{name} pixel at x:{closest_x} y:{closest_y}")
+            self.targets.append((closest_x, closest_y))
+            return True
+        else:
+            return False
+        # for entry in order:
+        #     if entry[0] >= width or entry[1] >= height:
+        #         continue
+        #     r, g, b = minimap.getpixel((entry[0], entry[1]))
+        #     if inColorRange(r, g, b):
+        #         left, top, _w, _h = MINIMAP_REGION
+        #         self.targetX = left + entry[0] - MINIMAP_CENTER_X
+        #         self.targetY = top + entry[1] - MINIMAP_CENTER_Y
+        #         return True
+        # self.targetX = 0
+        # self.targetY = 0
+        # return False
+
+    def get_game_coords(self, target_found: bool = False, pathfind: bool = False) -> tuple[int, int, int]:
         """
         Translates relative minimap coordinates of target into game coordinates.
 
@@ -208,41 +215,107 @@ class Minimap:
             self.targetY = y - MINIMAP_CENTER_Y + 4
             print(f"jump icon at x: {self.targetX} y: {self.targetY}")
             return True
+        return False
 
 
-def spiralSearch(rows, cols, rStart, cStart) -> list[list]:
-    ans = []
-    end = rows * cols
-    i = i1 = i2 = rStart
-    j = j1 = j2 = cStart
-    while True:
-        j2 += 1
-        while j < j2:
-            if 0 <= j < cols and 0 <= i:
-                ans.append([i, j])
-            j += 1
-            if 0 > i:
-                j = j2
-        i2 += 1
-        while i < i2:
-            if 0 <= i < rows and j < cols:
-                ans.append([i, j])
-            i += 1
-            if j >= cols:
-                i = i2
-        j1 -= 1
-        while j > j1:
-            if 0 <= j < cols and i < rows:
-                ans.append([i, j])
-            j -= 1
-            if i >= rows:
-                j = j1
-        i1 -= 1
-        while i > i1:
-            if 0 <= i < rows and 0 <= j:
-                ans.append([i, j])
-            i -= 1
-            if 0 > j:
-                i = i1
-        if len(ans) == end:
-            return ans
+def distanceBetweenCoordinates(coord1: tuple[int, int], coord2: tuple[int, int]) -> int:
+    """
+    Calculates the distance between two coordinates.
+    """
+    target_dist = math.sqrt((coord1[0] - coord2[0]) ** 2 + (coord1[1] - coord2[1]) ** 2)
+    return int(target_dist)
+
+
+def average_coordinate(coords: list[tuple[int, int]]) -> tuple[int, int]:
+    """
+    Calculate the average coordinate from a list of coordinates.
+    """
+    xs = [coord[0] for coord in coords]
+    ys = [coord[1] for coord in coords]
+    meanX = int(sum(xs) / len(xs))
+    meanY = int(sum(ys) / len(ys))
+    return meanX, meanY
+
+
+def get_adjacent_coordinates(coord: tuple[int, int]):
+    """
+    Return a list of adjacent coordinates (4-directional).
+    """
+    x, y = coord
+    return [(x + dx, y + dy) for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]]
+
+
+def closest_connected_coordinate(
+    valid_coords: list[tuple[int, int]], target: tuple[int, int]
+) -> tuple[int, int]:
+    """
+    Find the coordinate closest to the origin that is connected to the target.
+    """
+
+    queue = deque([target])
+    visited = []
+    closest_coord = None
+    min_distance = float("inf")
+
+    while queue:
+        current = queue.popleft()
+
+        if current in visited:
+            continue
+
+        visited.append(current)
+
+        if current in valid_coords:
+            distance = distanceBetweenCoordinates(current, (0, 0))
+            if distance < min_distance:
+                min_distance = distance
+                closest_coord = current
+
+        for neighbor in get_adjacent_coordinates(current):
+            if neighbor not in visited and neighbor in valid_coords:
+                queue.append(neighbor)
+
+    return closest_coord
+
+
+def MOG_RGB_RANGE(r: int, g: int, b: int) -> bool:
+    if config["GFN"]:
+        return 180 < r < 215 and 17 < g < 35 and 17 < b < 55
+    else:
+        return 180 < r < 215 and 17 < g < 35 and 17 < b < 55
+
+
+def ELITE_RGB_RANGE(r: int, g: int, b: int) -> bool:
+    if config["GFN"]:
+        return 184 < r < 215 and 124 < g < 147 and 59 < b < 78
+    else:
+        return 189 < r < 215 and 124 < g < 150 and 29 < b < 70
+
+def BOSS_RGB_RANGE(r: int, g: int, b: int) -> bool:
+    if config["GFN"]:
+        return 100 < r < 170 and g < 35 and b < 35
+    else:
+        return 100 < r < 170 and g < 35 and b < 35
+
+def BUFF_RGB_RANGE(r: int, g: int, b: int) -> bool:
+    if config["GFN"]:
+        return 210 < r < 245 and 170 < g < 190 and 30 < b < 50 and r - g > 40
+    else:
+        return 200 < r < 255 and 170 < g < 200 and 30 < b < 70
+
+
+def VALID_AREA_RGB_RANGE(r: int, g: int, b: int) -> bool:
+    return (130 < r < 165 and 140 < g < 160 and 125 < b < 150) or (
+        140 < r < 150 and 130 < g < 140 and 115 < b < 125
+    )
+
+
+def PORTAL_RGB_RANGE(r: int, g: int, b: int) -> bool:
+    if config["GFN"]:
+        return (75 < r < 105 and 140 < g < 170 and 240 < b < 256) or (
+            120 < r < 130 and 210 < g < 240 and 240 < b < 256
+        )
+    else:
+        return (75 < r < 85 and 140 < g < 150 and 250 < b < 256) or (
+            120 < r < 130 and 210 < g < 220 and 250 < b < 256
+        )
