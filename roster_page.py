f0b8aad34dbd3b6f02b7fa2ee02b58b84a77d783
Zach Huang <zhuo-nanhuang2024@u.northwestern.edu>
2024-10-27 06:42:40 -0400

    implement roster slot switching using editable indexes

--- roster_page.py
+++ roster_page.py
@@ -75,16 +75,21 @@ def load_roster():
     spec.loader.exec_module(roster_module)
     return roster_module.roster
 
+
 roster = load_roster()
 
+
 def save_roster():
+    global roster
+    roster = sorted(roster, key=lambda char: char["index"])
     with open("configs/roster.py", "w") as f:
         f.write("roster = " + repr(roster))
 
     ui.notify("Roster saved successfully!")
-
+    roster_layout.refresh()
 
 def add_char_to_roster():
+    global roster
     new_entry = {
         "index": len(roster),
         "class": None,
@@ -95,45 +100,50 @@ def add_char_to_roster():
     roster.append(new_entry)
     roster_layout.refresh()
 
+
 def reload_roster():
     global roster
     roster = load_roster()
     roster_layout.refresh()
 
+
 @ui.refreshable
 def roster_layout():
-    with ui.grid(columns=3).classes('gap-5'):
+    with ui.grid(columns=3).classes("gap-5"):
         for i in range(len(roster)):
-            with ui.card().classes('w-auto m-auto') as card:
-                ui.input(
-                    label="Name"
-                ).bind_value(roster[i], "name").classes('w-11/12 m-auto')
-                ui.select(
-                    options=CLASS_NAMES, label="Class", with_input=True
-                ).bind_value(roster[i], "class").classes('w-11/12 m-auto')
-                ui.select(
-                    options=DUNGEON_ITEM_LEVELS,
-                    label="Dungeon Item Level",
-                    with_input=True,
-                    clearable=True,
-                ).bind_value(roster[i], "chaosItemLevel").classes('w-11/12 m-auto')
-                ui.select(
-                    options=UNA_TASK_NAMES, label="Unas", with_input=True, multiple=True
-                ).bind_value(roster[i], "unas").classes('w-11/12 m-auto').props(
-                    "use-chips"
-                )
-                ui.switch(
-                    text="Guild Donation",
-                ).bind_value(
-                    roster[i], "guildDonation"
-                ).classes('m-auto')
+            character_card(roster[i])
 
         with ui.card():
             ui.button("Add new character", on_click=add_char_to_roster).style(
                 "margin: auto"
             )
 
-class CharacterDeleter():
+
+def character_card(char: dict):
+    with ui.card().classes("w-auto m-auto") as card:
+        with ui.row().classes("w-11/12 m-auto"):
+            ui.input(label="Name").bind_value(char, "name").classes("w-8/12 m-auto")
+            ui.select(options=list(range(len(roster))), label="#").bind_value(char, "index").classes("w-3/12 m-auto")
+        ui.select(options=CLASS_NAMES, label="Class", with_input=True).bind_value(
+            char, "class"
+        ).classes("w-11/12 m-auto")
+        ui.select(
+            options=DUNGEON_ITEM_LEVELS,
+            label="Dungeon Item Level",
+            with_input=True,
+            clearable=True,
+        ).bind_value(char, "chaosItemLevel").classes("w-11/12 m-auto")
+        ui.select(
+            options=UNA_TASK_NAMES, label="Unas", with_input=True, multiple=True
+        ).bind_value(char, "unas").classes("w-11/12 m-auto").props("use-chips")
+        ui.switch(
+            text="Guild Donation",
+        ).bind_value(
+            char, "guildDonation"
+        ).classes("m-auto")
+
+
+class CharacterDeleter:
     target = ""
 
     @classmethod
@@ -141,22 +151,29 @@ class CharacterDeleter():
         global roster
         roster = [char for char in roster if char["name"] != cls.target]
         roster_layout.refresh()
-        
-        
-with ui.dialog() as confirmation_dialog, ui.card():
-    ui.label('Enter the character name you wish to delete.')
-    
-    ui.input('Character Name').bind_value_to(CharacterDeleter, "target")
-    with ui.row().style('justify-content: flex-end;'):
-        ui.button('Confirm', on_click=lambda: [CharacterDeleter.delete_character(), confirmation_dialog.close()]).props('color=red')
-        ui.button('Cancel', on_click=confirmation_dialog.close).props('color=grey')
+
+
+with ui.dialog() as delete_character_popup, ui.card():
+    ui.label("Enter the character name you wish to delete.")
+
+    ui.input("Character Name").bind_value_to(CharacterDeleter, "target")
+    with ui.row().style("justify-content: flex-end;"):
+        ui.button(
+            "Confirm",
+            on_click=lambda: [
+                CharacterDeleter.delete_character(),
+                delete_character_popup.close(),
+            ],
+        ).props("color=red")
+        ui.button("Cancel", on_click=delete_character_popup.close).props("color=grey")
+
 
 def roster_page():
     with ui.row():
         ui.button("Save Roster", on_click=save_roster)
         ui.button("Reload Roster", on_click=lambda: reload_roster())
-        # ui.button("Print Roster", on_click=lambda: print(roster))
-        ui.button("Delete Character", on_click=confirmation_dialog.open).props('color=red')
+        ui.button("Delete Character", on_click=delete_character_popup.open).props(
+            "color=red"
+        )
 
-    # Display area for the roster
     roster_layout()
