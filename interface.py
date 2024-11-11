9291f1eccb31ef61ac95a694e7c4ea41bdcc7de7
Zach Huang <zhuo-nanhuang2024@u.northwestern.edu>
2024-11-10 19:47:43 -0500

    suppress NiceGUI binding propagation warnings

--- interface.py
+++ interface.py
@@ -1,4 +1,6 @@
 from nicegui import ui
+import nicegui.binding
+
 
 from interface_pages.home_page import home_page
 from interface_pages.roster_page import roster_page
@@ -6,6 +8,8 @@ from interface_pages.skills_page import skills_page
 from interface_pages.configs_page import configs_page
 from interface_pages.run_page import run_page
 from interface_pages.setup_examples_page import setup_examples_page
+
+nicegui.binding.MAX_PROPAGATION_TIME = 0.1
 
 with ui.dialog() as dialog, ui.card():
     result = ui.markdown()
@@ -43,11 +47,12 @@ with ui.page_sticky(position="bottom-right", x_offset=20, y_offset=20):
     ui.button(on_click=ui.dark_mode().toggle, icon="dark_mode").props("fab")
 
 ui.run(
-    # native=True,
+    # native=True, # pywebview and pythonnet not supported in python 3.13
     title='ZephBot',
     favicon='favicon.ico',
     # window_size=(960, 540),
     fullscreen=False,
     dark=True,
+    reload=False,
     uvicorn_reload_excludes="configs/*",
 )
